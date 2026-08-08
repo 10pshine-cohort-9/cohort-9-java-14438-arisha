76fc802f6ab30359610c42fedc5bdfb8e3d6b7c4
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-08 12:37:12 -0700

    Add successful registration unit test

--- backend/src/test/java/com/contactmanagement/backend/AuthServiceTest.java
+++ backend/src/test/java/com/contactmanagement/backend/AuthServiceTest.java
@@ -7,6 +7,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.contactmanagement.backend.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.mockito.InjectMocks;
+import com.contactmanagement.backend.service.AuthService;
+import com.contactmanagement.backend.dto.RegisterRequest;
+import static org.mockito.Mockito.when;
+import com.contactmanagement.backend.entity.User;
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
@@ -16,10 +25,36 @@ public class AuthServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
+    @InjectMocks 
+    private AuthService authService;
+
     //1. User Registration Test
     @Test 
     void registerUserSuccessfully(){
+        // registration request with email only
+        RegisterRequest request = new RegisterRequest("Test User", "test@example.com", null, "password123");
 
+        // if database does not contain email
+        when(userRepository.existsByEmail("test@example.com")).thenReturn(false);
+
+        // When password is entered, hash version is returned
+        when(passwordEncoder.encode("password123")).thenReturn("hashedPassword");
+
+        // repository would store registered user
+        User savedUser = new User("Test User", "test@example.com", null, "hashedPassword");
+
+        //saved user will be returned by repository
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        User result = authService.register(request);
+        assertNotNull(result);
+
+        // Test values
+        assertEquals("Test User", result.getFullName());
+        assertEquals("test@example.com", result.getEmail());
+        assertEquals("hashedPassword", result.getPasswordHash());
+
+        verify(userRepository).save(any(User.class));
     }
     
 }
