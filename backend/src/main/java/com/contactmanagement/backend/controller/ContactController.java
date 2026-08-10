2275dcc9500b8eeda1546e922c1503a7f9016081
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-10 11:29:25 -0700

    Add get contact by id endpoint

--- backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
+++ backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
@@ -1,13 +1,16 @@
 package com.contactmanagement.backend.controller;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.contactmanagement.backend.service.ContactService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import com.contactmanagement.backend.entity.Contact;
+import com.contactmanagement.backend.service.ContactService;
 
 @RestController
 @RequestMapping("/api/contacts")
@@ -19,9 +22,16 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    //1. fetch all contacts- CRUD action: GET /api/contacts
     @GetMapping
     public Page<Contact> getAllContacts(Pageable pageable){
         return contactService.getAllContacts(pageable);
+    }
+
+    //2. fetch contacts by id- CRUD action: Get /api/contact/5
+    @GetMapping("/{id}")
+    public Optional<Contact> getContactById(@PathVariable Integer id){
+        return contactService.getContactById(id);
     }
 
 }
