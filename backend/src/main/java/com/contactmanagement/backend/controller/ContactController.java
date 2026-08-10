582cab38e6d842f8f70aff3832d831235f86eef2
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-10 11:46:39 -0700

    Add create contact endpoint

--- backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
+++ backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
@@ -4,13 +4,17 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.contactmanagement.backend.entity.Contact;
 import com.contactmanagement.backend.service.ContactService;
+import com.contactmanagement.backend.entity.User;
 
 @RestController
 @RequestMapping("/api/contacts")
@@ -28,10 +32,17 @@ public class ContactController {
         return contactService.getAllContacts(pageable);
     }
 
-    //2. fetch contacts by id- CRUD action: Get /api/contact/5
+    //2. fetch contacts by id- CRUD action: Get /api/contacts/5
     @GetMapping("/{id}")
     public Optional<Contact> getContactById(@PathVariable Integer id){
         return contactService.getContactById(id);
+    }
+
+    //3. Creating a contact- CRUD action: POST /api/contacts
+    @PostMapping
+    public Contact createContact(@RequestBody Contact contact, @AuthenticationPrincipal User user){
+        contact.setUser(user);
+        return contactService.saveContact(contact);
     }
 
 }
