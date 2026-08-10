c451976df91fc30e989076bd3e37d98076a16926
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-10 11:21:17 -0700

    Add get all contacts endpoint

--- backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
+++ backend/src/main/java/com/contactmanagement/backend/controller/ContactController.java
@@ -1,9 +1,13 @@
 package com.contactmanagement.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.contactmanagement.backend.service.ContactService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.contactmanagement.backend.entity.Contact;
 
 @RestController
 @RequestMapping("/api/contacts")
@@ -13,6 +17,11 @@ public class ContactController {
 
     public ContactController(ContactService contactService) {
         this.contactService = contactService;
+    }
+
+    @GetMapping
+    public Page<Contact> getAllContacts(Pageable pageable){
+        return contactService.getAllContacts(pageable);
     }
 
 }
