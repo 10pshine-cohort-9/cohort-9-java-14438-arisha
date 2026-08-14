6291f83c091af977d7cab2868d7f99ecb3fecf57
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-14 04:43:21 -0700

    Add contact phone service structure

--- backend/src/main/java/com/contactmanagement/backend/service/ContactPhoneService.java
+++ backend/src/main/java/com/contactmanagement/backend/service/ContactPhoneService.java
@@ -2,7 +2,16 @@ package com.contactmanagement.backend.service;
 
 import org.springframework.stereotype.Service;
 
+import com.contactmanagement.backend.repository.ContactPhoneRepository;
+
 @Service
 public class ContactPhoneService {
+    private final ContactPhoneRepository contactPhoneRepository;
+    private final ContactService contactService;
+
+    public ContactPhoneService(ContactPhoneRepository contactPhoneRepository, ContactService contactService) {
+        this.contactPhoneRepository = contactPhoneRepository;
+        this.contactService = contactService;
+    }
     
 }
