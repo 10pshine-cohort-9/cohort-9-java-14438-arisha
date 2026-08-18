d7abcf73e36766a29d17c8bb7029eaaeaa971c4e
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-18 09:21:39 -0700

    Handle database constraint violations

--- backend/src/main/java/com/contactmanagement/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/contactmanagement/backend/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.contactmanagement.backend.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Database constraint violation");
+    }
 }
