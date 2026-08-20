476cf53806d247e5befe66bb32dcdd3653f2a76f
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-19 23:33:20 -0700

    Add get contact by ID test

--- backend/src/test/java/com/contactmanagement/backend/ContactServiceTest.java
+++ backend/src/test/java/com/contactmanagement/backend/ContactServiceTest.java
@@ -1,10 +1,18 @@
 package com.contactmanagement.backend;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.contactmanagement.backend.entity.Contact;
 import com.contactmanagement.backend.repository.ContactRepository;
 import com.contactmanagement.backend.service.ContactService;
 
@@ -15,5 +23,26 @@ public class ContactServiceTest {
 
     @InjectMocks
     private ContactService contactService;
+
+    //Case 1: Successfully getting correct contact by id for correct user
+    @Test
+    void getContactByIdSuccessfully() {
+
+        //Give sample contact details
+        Contact contact = new Contact();
+        contact.setId(1);
+        contact.setFirstName("Ali");
+        contact.setLastName("Khan");
+
+        when(contactRepository.findByIdAndUserId(1, 10)).thenReturn(Optional.of(contact));
+        Optional<Contact> result = contactService.getContactById(1, 10); // call service
+
+        //Returned values
+        assertTrue(result.isPresent());
+        assertEquals("Ali", result.get().getFirstName());
+        assertEquals("Khan", result.get().getLastName());
+
+        verify(contactRepository).findByIdAndUserId(1, 10);
+    } 
     
 }
