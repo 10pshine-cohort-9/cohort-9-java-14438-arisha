53d6888d3823e04bd109992773fa3b5c1a65d981
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-24 06:21:33 -0700

    Remove unnecessary security exception declaration

--- backend/src/main/java/com/contactmanagement/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/contactmanagement/backend/config/SecurityConfig.java
@@ -20,9 +20,7 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    @SuppressWarnings("java:S112")
     @Bean
-    // Spring Security's HttpSecurity build process declares Exception.
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
