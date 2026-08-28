3a53ff4cbe333a5d3c25c63ad4f147776988404d
Arisha Fatima Talpur <arishatalpur411@gmail.com>
2026-08-28 09:37:43 -0700

    Add profile page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ContactsPage from "./pages/ContactsPage";
 import ContactDetailsPage from "./pages/ContactDetailsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/contacts" element={<ContactsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/contacts/:id" element={<ContactDetailsPage />} />
             </Routes>
         </BrowserRouter>
---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import DashboardLayout from "../components/DashboardLayout";
+
+function ProfilePage() {
+    return (
+        <DashboardLayout
+            title="Profile"
+            subtitle="View and manage your account information."
+        >
+            <p>Profile details will appear here.</p>
+        </DashboardLayout>
+    );
+}
+
+export default ProfilePage;
